ffd126b2e4b79eea93e6c1d412e7424a3dae1f48
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-17 10:06:04 +0100

    .env file with personal data created configuration.js file with code updated for .env

--- src/server/db/configuration.js
+++ src/server/db/configuration.js
@@ -1,6 +1,13 @@
+const dotenv = require('dotenv').config({path:'C:/Projects/BooketList/.env'})
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_DATABASE = process.env.DB_DATABASE
+
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Orquideapurpura6',
-  database: 'booketlist',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_DATABASE
 }
