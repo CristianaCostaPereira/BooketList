06e8d4bae02e1e14c5f2ae89ddbf8ced37ae314e
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-19 02:12:31 +0100

    login component ongoing

--- src/client/booketlist/src/components/login.vue
+++ src/client/booketlist/src/components/login.vue
@@ -1,76 +1,151 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="344"
-    outlined
+    style="max-width: 500px; margin-top:10px"
   >
-    <v-form>
-    <v-container fluid>
-      <v-row>
-        <v-col
-          cols="12"
-          sm="12"
-        >
-          <v-text-field
-            v-model="password"
-            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
-            :type="show1 ? 'text' : 'password'"
-            name="input-10-1"
-            label="Normal with hint text"
-            hint="At least 8 characters"
-            counter
-            @click:append="show1 = !show1"
-          ></v-text-field>
-        </v-col>
+    <v-toolbar
+      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+      cards
+      dark
+      flat
+    >
+      <v-card-title class="text-h6 font-weight-regular">
+        Sign Up
+      </v-card-title>
 
-            <v-col
-          cols="12"
-          md="12"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
+      <v-spacer></v-spacer>
 
-        
-      </v-row>
-    </v-container>
-  </v-form>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-form
+      ref="form"
+      v-model="form"
+      class="pa-4 pt-6"
+    >      
+      <v-text-field
+        v-model="email"
+        :rules="[rules.email]"
+        filled
+        color="deep-purple"
+        label="Email address"
+        type="email"
+      >
+      </v-text-field>
+
+      <v-text-field
+        v-model="password"
+        :rules="[rules.password, rules.length(6)]"
+        filled
+        color="deep-purple"
+        counter="6"
+        label="Password"
+        style="min-height: 96px"
+        type="password"
+      >
+      </v-text-field>
+
+      <v-checkbox
+        v-model="agreement"
+        :rules="[rules.required]"
+        color="deep-purple"
+      >
+        <template v-slot:label>
+          I agree to the&nbsp;
+          <a
+            href="#"
+            @click.stop.prevent="dialog = true"
+          >Terms of Service</a>
+          &nbsp;and&nbsp;
+          <a
+            href="#"
+            @click.stop.prevent="dialog = true"
+          >Privacy Policy</a>*
+        </template>
+      </v-checkbox>
+    </v-form>
+
+    <v-divider></v-divider>
 
     <v-card-actions>
       <v-btn
-        outlined
-        rounded
         text
+        @click="$refs.form.reset()"
       >
-        Button
+        Clear
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :disabled="!form"
+        :loading="isLoading"
+        class="white--text"
+        color="deep-purple accent-4"
+        depressed
+      >
+        Submit
       </v-btn>
     </v-card-actions>
+
+    <v-dialog
+      v-model="dialog"
+      absolute
+      max-width="400"
+      persistent
+    >
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-3" src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg">
+          Legal
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn
+            text
+            @click="agreement = false, dialog = false"
+          >
+            No
+          </v-btn>
+
+          <v-spacer></v-spacer>
+          
+          <v-btn
+            class="white--text"
+            color="deep-purple accent-4"
+            @click="agreement = true, dialog = false"
+          >
+            Yes
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        show1: false,
-        password: 'Password',
-        rules: {
-          required: value => !!value || 'Required.',
-          min: v => v.length >= 8 || 'Min 8 characters',
-          emailMatch: () => (`The email and password you entered don't match`),
-          
-        },
-        email: '',
-        emailRules: [
-          v => !!v || 'E-mail is required',
-          v => /.+@.+/.test(v) || 'E-mail must be valid',
-        ],
-      }
-    },
+    data: () => ({
+      agreement: false,
+      dialog: false,
+      email: undefined,
+      form: false,
+      isLoading: false,
+      password: undefined,
+      rules: {
+        email: v => !!(v || '').match(/@/) || 'Please enter a valid email',
+        length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
+        password: v => !!(v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
+          'Password must contain an upper case letter, a numeric character, and a special character',
+        required: v => !!v || 'This field is required',
+      },
+    }),
   }
 </script>
