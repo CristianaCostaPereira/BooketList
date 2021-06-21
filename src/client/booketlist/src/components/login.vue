f975c5159b27fee047bddc167df20b9de9bca792
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-21 23:13:05 +0100

    store token in localStorage

--- src/client/booketlist/src/components/login.vue
+++ src/client/booketlist/src/components/login.vue
@@ -151,9 +151,19 @@
 
     methods: {
       async login() {
+
+        let data = {
+          email: 'cris@gmail.com',
+          password: '123'
+        }
+
         try {
-          const response = await axios.post('http://localhost:3000/login');
-          console.log(response);
+          const response = await axios.post('http://localhost:3000/login', data)
+          console.log(response.data)
+          window.localStorage.setItem('book-token', response.data) // Para armazenar o token sem o perder quando mudar a rota/componente
+
+          // window.localStorage.getItem('book-token')
+
         } catch (error) {
           console.error(error);
         }
