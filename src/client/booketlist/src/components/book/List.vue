962b3f8523ff83fc9fb1cf07552a6c714625affb
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-22 16:32:53 +0100

    List second option

--- src/client/booketlist/src/components/book/List.vue
+++ src/client/booketlist/src/components/book/List.vue
@@ -9,6 +9,24 @@
         md="4"
         lg="3"
       >
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img :src="volumeInfo.imageLinks.smallThumbnail" alt="sans" style="margin-left: 15px; margin-top: 20px;">
+            </div>
+          
+            <div class="col-md-8">
+              <div class="card-body ml-4">
+                <v-icon color="red lighten-3" style="margin-left: 190px;">mdi-heart</v-icon>
+                
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <v-card
           max-width="300"
           class="mx-auto mt-4"
